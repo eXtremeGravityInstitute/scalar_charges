7406045c1ecf6e967ef2730f4113222d40be4359
David Anderson <dsa9525@Davids-MacBook-Pro-2.local>
2019-01-07 11:17:17 -0700

    adding thrid batch of files

---
+++ DEF/WFF1/concat.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+tar -xzf WFF1_1.tar.gz
+tar -xzf WFF1_2.tar.gz
+tar -xzf WFF1_3.tar.gz
+cat xa* > DEF_WFF1_full_data.dat
+rm xa*
---
+++ DEF/concat_all.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+cd AP3 
+./concat.sh
+cd ../
+cd AP4 
+./concat.sh
+cd ../
+cd ENG 
+./concat.sh
+cd ../
+cd H4 
+./concat.sh
+cd ../
+cd MPA1 
+./concat.sh
+cd ../
+cd MS0 
+./concat.sh
+cd ../
+cd MS2 
+./concat.sh
+cd ../
+cd PAL1 
+./concat.sh
+cd ../
+cd SLy4 
+./concat.sh
+cd ../
+cd WFF1 
+./concat.sh
+cd ../
+cd WFF2 
+./concat.sh
+cd ../
